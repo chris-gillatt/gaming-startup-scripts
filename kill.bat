6c95135610304c37570327ae0e5ab2bfb95b8206
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2020-08-18 19:28:14 +0100

    initial commit

---
+++ kill.bat
@@ -0,0 +1,6 @@
+@
+
+taskkill /im "Discord.exe" /t /f 
+taskkill /im "Battle.net.exe" /t /f 
+
+pause 
---
+++ start.bat
@@ -0,0 +1,5 @@
+@
+
+C:\Users\Chris\AppData\Local\Discord\Update.exe --processStart Discord.exe
+start "" "C:\Program Files (x86)\MSI\Gaming APP\SGamingApp.exe"
+start "" "C:\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
