85734f4344d6eef148f78a0422cdd7c5e7edf6fb
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2020-10-18 12:18:40 +0100

    Start Firefox as well

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
-@
+@ECHO OFF
 
 C:\Users\Chris\AppData\Local\Discord\Update.exe --processStart Discord.exe
 start "" "C:\Program Files (x86)\MSI\Gaming APP\SGamingApp.exe"
 start "" "C:\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
 start "" "C:\Users\Chris\AppData\Local\Programs\signal-desktop\Signal.exe"
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
