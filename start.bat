738a73683eb92758f3dae74792e9b59d25ac08b4
Chris Gillatt <solex@hotmail.co.uk>
2021-01-28 19:27:48 +0000

    correct location for discord

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
-C:\Users\Chris\AppData\Local\Discord\Update.exe --processStart Discord.exe
-start "" "C:\Program Files (x86)\MSI\Gaming APP\SGamingApp.exe"
+C:\Users\solex\AppData\Local\Discord\Update.exe --processStart Discord.exe
 start "" "C:\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
 start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
 :: start "" "C:\WINDOWS\system32\SnippingTool.exe"
