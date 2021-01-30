50c9bc5fd7b0c3cc0323177c83160299133050a4
Chris Gillatt <solex@hotmail.co.uk>
2021-01-30 20:00:33 +0000

    add signal to start

--- start.bat
+++ start.bat
@@ -2,3 +2,4 @@ C:\Users\solex\AppData\Local\Discord\Update.exe --processStart Discord.exe
 start "" "C:\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
 start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
 :: start "" "C:\WINDOWS\system32\SnippingTool.exe"
+start "" "C:\Users\solex\AppData\Local\Programs\signal-desktop\Signal.exe"
